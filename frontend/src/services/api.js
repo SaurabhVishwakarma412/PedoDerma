d3a689f9bcbd208b8ebbf3b5814af180a262e281
SaurabhVishwakarma412 <saurabhkv412@gmail.com>
2026-03-23 03:13:33 +0530

    update api url to render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,32 +1,29 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pedoderma-backend.onrender.com/api",
   withCredentials: true,
 });
 
 // REQUEST INTERCEPTOR
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem("token");
-    
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 // RESPONSE INTERCEPTOR
-api.interceptors.response.use((response) => response,
+api.interceptors.response.use(
+  (response) => response,
   (error) => {
-    console.error(
-      "API ERROR:",
-      error?.response?.data || error.message
-    );
+    console.error("API ERROR:", error?.response?.data || error.message);
     return Promise.reject(error);
   }
 );
